4da33f7c7419f4783af834767ef91b6f6da3f084
Nikolay Botev <nbotev@Nikolays-Air.lan>
2019-02-14 20:03:22 -0800

    Add install.sh

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Various files for bootsrapping a new computer with decent config.
+Setup a new MacOS user account for development in seconds.
+
+```
+sh -c "$(curl -fsSL https://raw.github.com/nikolaybotevb/bootstrap/master/install.sh)"
+```
 
 # Vim Plugins
 
---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Preconditions
+if [ "$(uname)" != 'Darwin' ]; then echo "This script only runs on macOS."; exit 1; fi
+if [ -d ~/.bootstrap ]; then echo "~/.bootstrap directory already exists."; exit 1; fi
+
+
+# get repo
+git clone https://github.com/nikolaybotevb/bootstrap.git ~/.bootstrap
+
+
+# git config
+user_fullname="$(dscacheutil -q user -a name "$(whoami)" |awk '$1 == "gecos:" { print $2 " " $3 }')"
+user_email="$(dscl . readpl "$HOME" dsAttrTypeNative:LinkedIdentity appleid.apple.com:linked\ identities:0:full\ name | awk '{print $4}')"
+echo "Git Name: $user_fullname"
+echo "Git E-mail: $user_email"
+print -n "Press return to continue . . ." && read
+sed -e "s/_name_/${user_fullname}/g; s/_email_/${user_email}/g" ~/.bootstrap/.gitconfig > ~/.gitconfig
+cp ~/.bootstrap/.gitignore ~
+
+
+# .vimrc
+cp ~/.bootstrap/.vimrc ~
+
+
+# zsh (https://ohmyz.sh)
+cp ~/.bootstrap/.aliases ~
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed -i "" -e "s/^# CASE_SENSITIVE=/CASE_SENSITIVE=/" ~/.zshrc
+sed -i "" -e "s/^# DISABLE_UNTRACKED_FILES_DIRTY=/DISABLE_UNTRACKED_FILES_DIRTY=/" ~/.zshrc
+echo "\nsource ~/.aliases" >> ~/.zshrc
+
+# git graphical diff tools
+open https://sourceforge.net/projects/kdiff3/files/
+open https://www.perforce.com/downloads/visual-merge-tool
+open https://www.scootersoftware.com/download.php
+
+
+# other standard tools to install manually: macports, homebrew, jenv, nvm
+open https://www.macports.org/install.php
+open https://brew.sh
+open http://www.jenv.be
+oepn https://github.com/creationix/nvm#installation
+
