c12d91942c48e92bbf79c4d118aa56fcbec6ee85
Nikolay Botev <nikolaybotev.b@gmail.com>
2021-06-10 21:35:10 -0400

    Copy OS .zshrc_local template if available on install

--- install.sh
+++ install.sh
@@ -64,6 +64,7 @@ git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 # Configure zsh
 echo "Configuring zsh ..."
 cp ~/.bootstrap/.zshrc ~
+[ -f "~/.bootstrap/$(uname).zshrc_local" ] && cp "~/.bootstrap/$(uname).zshrc_local" ~/.zshrc_local
 
 
 # Open Websites for Tools to install manually
