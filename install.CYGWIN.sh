6c1404de8a1b9adbde1902f1c690581103404443
Nikolay Botev <nikolaybotev.b@gmail.com>
2024-10-18 09:49:43 +0300

    Update install.CYGWIN.sh

--- install.CYGWIN.sh
+++ install.CYGWIN.sh
@@ -1,2 +1,4 @@
-# Prompt
-eval "$(starship init zsh)"
+#!/bin/sh -e
+
+# Check Preconditions
+if [ "$(uname | awk -F_ '{print $1}')" != 'CYGWIN' ]; then echo "This script only runs on Cygwin."; exit 1; fi
