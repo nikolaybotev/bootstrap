585cbf5649b4f96097c4336b6cae52d695041eb1
Nikolay Botev <nikolaybotev.b@gmail.com>
2024-10-30 11:44:54 +0200

    Update install.CYGWIN.sh

--- install.CYGWIN.sh
+++ install.CYGWIN.sh
@@ -2,3 +2,6 @@
 
 # Check Preconditions
 if [ "$(uname | awk -F_ '{print $1}')" != 'CYGWIN' ]; then echo "This script only runs on Cygwin."; exit 1; fi
+
+# Prompt zsh SHELL config
+echo "Please set SHELL to /bin/zsh in you Windows Account Environment Variables" 
