5b2b0ccd36f9dcdef2664e6303ed1e4943a95f5b
Nikolay Botev <nikolaybotev.b@gmail.com>
2025-12-26 11:26:22 -0800

    Harden the Linux installation script for Alpine, Ubuntu and Fedora docker images.

--- install.Linux.sh
+++ install.Linux.sh
@@ -4,7 +4,11 @@
 if [ "$(uname)" != 'Linux' ]; then echo "This script only runs on Linux."; exit 1; fi
 
 
-# Install and Select zsh
-[ -f /usr/bin/yum ] && sudo yum install util-linux-user zsh
-[ -f /usr/bin/apt ] && sudo apt install zsh
-sudo chsh -s /bin/zsh "$USER"
+# Install and Select zsh (and prerequisites)
+[ -f /usr/bin/yum ] && [ "$(whoami)" = "root" ] && yum install -y sudo
+[ -f /usr/bin/yum ] && sudo yum install -y util-linux-user zsh git shadow
+[ -f /usr/bin/apt-get ] && [ "$(whoami)" = "root" ] && apt-get update && apt-get install -y sudo
+[ -f /usr/bin/apt-get ] && sudo apt-get update && sudo apt-get install -y zsh git shadow
+[ -f /sbin/apk ] && [ "$(whoami)" = "root" ] && apk add --no-cache sudo
+[ -f /sbin/apk ] && sudo apk add --no-cache zsh git shadow
+sudo chsh -s /bin/zsh "${USER:-$(whoami)}"
