cb4abc199914ed6d2d0ed7a2592dfe268908ab7c
Nikolay Botev <nikolaybotev.b@gmail.com>
2025-12-26 20:29:41 -0800

    Add gnupg to Linux packages.

--- install.Linux.sh
+++ install.Linux.sh
@@ -6,9 +6,9 @@ if [ "$(uname)" != 'Linux' ]; then echo "This script only runs on Linux."; exit 
 
 # Install and Select zsh (and prerequisites)
 [ -f /usr/bin/yum ] && [ "$(whoami)" = "root" ] && yum install -y sudo
-[ -f /usr/bin/yum ] && sudo yum install -y util-linux-user zsh git shadow
+[ -f /usr/bin/yum ] && sudo yum install -y util-linux-user zsh git gnupg shadow
 [ -f /usr/bin/apt-get ] && [ "$(whoami)" = "root" ] && apt-get update && apt-get install -y sudo
-[ -f /usr/bin/apt-get ] && sudo apt-get update && sudo apt-get install -y zsh git shadow
+[ -f /usr/bin/apt-get ] && sudo apt-get update && sudo apt-get install -y zsh git gnupg shadow
 [ -f /sbin/apk ] && [ "$(whoami)" = "root" ] && apk add --no-cache sudo
-[ -f /sbin/apk ] && sudo apk add --no-cache zsh git shadow
+[ -f /sbin/apk ] && sudo apk add --no-cache zsh git gnupg shadow
 sudo chsh -s /bin/zsh "${USER:-$(whoami)}"
